d3e6ec9c1d5e0aac1f0f1a17031fbe206dd62f5f
chibikes <okoh_chibuike@yahoo.com>
2024-03-24 14:29:10 +0100

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,8 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await Supabase.initialize(
-    url: 'https://uaeddetfadjtcwjwotas.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhZWRkZXRmYWRqdGN3andvdGFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3NDM1MTUsImV4cCI6MjAxNjMxOTUxNX0.YylIvPrh-59xordtXe4Pw0kt_DQ7sVFO3We7XEpdxY4',
+    url: '',
+    anonKey: '',
   );
 
   runApp(
