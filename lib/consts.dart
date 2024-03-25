c8fdc7c0f4469e4e56dc8075e8b1cb9055736f9e
chibikes <okoh_chibuike@yahoo.com>
2024-03-25 13:20:28 +0100

    update consts.dart

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,6 +1,3 @@
-const anonKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhZWRkZXRmYWRqdGN3andvdGFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3NDM1MTUsImV4cCI6MjAxNjMxOTUxNX0.YylIvPrh-59xordtXe4Pw0kt_DQ7sVFO3We7XEpdxY4';
-
 const Map<String, String> countryCodes = {
   'Afghanistan': '+93',
   'Albania': '+355',
